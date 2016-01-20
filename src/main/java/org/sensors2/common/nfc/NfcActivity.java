6e43858daf619d1c52ff5d752c2e3f3524ed2c6b
deusanyjunior <dj@ime.usp.br>
2016-01-19 22:44:00 -0200

    Nfc class update.

--- src/main/java/org/sensors2/common/nfc/NfcActivity.java
+++ src/main/java/org/sensors2/common/nfc/NfcActivity.java
@@ -6,7 +6,7 @@ import android.nfc.NfcAdapter;
 import org.sensors2.common.dispatch.DataDispatcher;
 
 public interface NfcActivity {
+    String MIME_TEXT_PLAIN = "text/plain";
     DataDispatcher getDispatcher();
     NfcAdapter getNfcAdapter();
-    void handleIntent(Intent intent);
 }
